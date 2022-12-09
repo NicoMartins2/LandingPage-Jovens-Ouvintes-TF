d89b84832f23fe0464569a388ba0690acaec53a7
NicoMartins2 <nick.legal@outlook.com>
2022-12-09 16:24:10 -0300

    Mudando sistema do scroll

--- menu.js
+++ menu.js
@@ -3,13 +3,15 @@ const aboutUs_btn = document.getElementById("aboutUs_btn");
 const project_btn = document.getElementById("project_btn");
 const contact_btn = document.getElementById("contact_btn");
 // variaveis dos botoes do menu
+let buttonStylezed = null;
 
 window.addEventListener("scroll", (event) => {
-    var scroll = this.scrollY;
+    let scroll = this.scrollY;
     console.log(scroll);
-    if(scroll >= 0 && scroll < 1000 ){
+
+    if(scroll >= 0 && scroll < 1000 && buttonStylezed === 0){
         buttonsSelected(0);
-    }else if (scroll > 1000){
+    }else if (scroll > 1000 && buttonStylezed != 1){
         buttonsSelected(1);
     }
 })
@@ -19,20 +21,27 @@ function buttonsSelected(selected){
     const defaultColor = "#000000";
     const selectedColor = "#519b9b";
 
+    home_btn.style.color = defaultColor;
+    aboutUs_btn.style.color = defaultColor;
+    project_btn.style.color = defaultColor;
+    contact_btn.style.color = defaultColor;
+
     switch(selected){
         case 0:
             home_btn.style.color = selectedColor;
-            aboutUs_btn.style.color = defaultColor;
-            project_btn.style.color = defaultColor;
-            contact_btn.style.color = defaultColor;
 
         case 1:
-            home_btn.style.color = defaultColor;
             aboutUs_btn.style.color = selectedColor;
-            project_btn.style.color = defaultColor;
-            contact_btn.style.color = defaultColor;
+
+        case 2:
+            project_btn.style.color = selectedColor;
+
+        case 3:
+            contact_btn.style.color = selectedColor;
             
     }
+
+    buttonStylezed = selected;
 }
 
 function changeButton_Color(button, colored){
